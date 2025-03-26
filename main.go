5b0a9cee74658fd8f7a9618b52c6972f4f72e7e7
Jevon Teul <2022157000@ub.edu.bz>
2025-03-26 13:25:52 -0600

    Worker

--- main.go
+++ main.go
@@ -1,30 +1,64 @@
 // Filename: main.go
+// Purpose: This program demonstrates how to create a TCP network connection using Go
+
 package main
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"strconv"
+	"sync"
 	"time"
 )
 
+func worker(wg *sync.WaitGroup, tasks chan string, dialer net.Dialer) {
+	defer wg.Done()
+	maxRetries := 3
+	for addr := range tasks {
+		var success bool
+		for i := range maxRetries {
+			conn, err := dialer.Dial("tcp", addr)
+			if err == nil {
+				conn.Close()
+				fmt.Printf("Connection to %s was successful\n", addr)
+				success = true
+				break
+			}
+			backoff := time.Duration(1<<i) * time.Second
+			fmt.Printf("Attempt %d to %s failed. Waiting %v...\n", i+1, addr, backoff)
+			time.Sleep(backoff)
+		}
+		if !success {
+			fmt.Printf("Failed to connect to %s after %d attempts\n", addr, maxRetries)
+		}
+	}
+}
+
 func main() {
-	// define the target host and port we want to connect to
+
+	var wg sync.WaitGroup
+	tasks := make(chan string, 100)
+
 	target := "scanme.nmap.org"
-	port := 80
-	portStr := strconv.Itoa(port)
-	address := net.JoinHostPort(target, portStr)
+
 	dialer := net.Dialer{
 		Timeout: 5 * time.Second,
 	}
-	// attempt to establish a connection
-	// the Dial function is used to connect to a server
-	conn, err := dialer.Dial("tcp", address)
-	if err != nil {
-		log.Fatalf("Unable to connect to %s: %v", address, err)
-	}
-	defer conn.Close()
 
-	fmt.Printf("Connection to %s was successful\n", address)
+	workers := 100
+
+	for i := 1; i <= workers; i++ {
+		wg.Add(1)
+		go worker(&wg, tasks, dialer)
+	}
+
+	ports := 512
+
+	for p := 1; p <= ports; p++ {
+		port := strconv.Itoa(p)
+		address := net.JoinHostPort(target, port)
+		tasks <- address
+	}
+	close(tasks)
+	wg.Wait()
 }
